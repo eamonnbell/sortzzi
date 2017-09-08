428b13b17abcdda1686281a1ff833892213f1d8e
Eamonn Bell <pseudoboarder@gmail.com>
2017-09-08 13:47:39 +0100

    Actually add AlbumCard component...

---
+++ src/components/AlbumCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card">
+        <div class="card-content">
+            <div class="media">
+                <div class="media-left">
+                    <figure class="image">
+                        <img v-bind:src="item.images[2].url" alt="Image">
+                    </figure>
+                </div>
+                <div class="media-content">
+                    <p class="title is-4">{{ item.name }}</p>
+                    <p class="subtitle">
+                        <span class="tag" v-for="artistName in artistNames">{{ artistName }}</span>
+                    </p>
+                </div>
+            </div>
+
+            <div class="content">
+                Tracklisting goes here
+            </div>           
+        </div>
+         <footer class="card-footer">
+                <a class="card-footer-item" :href="item.external_urls.spotify">Play album</a>
+                <a class="card-footer-item">Like</a>
+                <a class="card-footer-item">Hate</a>
+            </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'albumcard',
+    props: ['item'],
+    computed: {
+        artistNames(){
+            var artistNames = this.item.artists.map(artist => artist.name);
+            return artistNames
+        }
+    }
+}
+</script>
+<style>
+
+</style>
