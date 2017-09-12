052f5b58c9f54828134cfa9d0ea7d93f692a53a7
Eamonn Bell <pseudoboarder@gmail.com>
2017-09-12 16:00:23 +0100

    Ready variables for prod

--- src/globals.js
+++ src/globals.js
@@ -10,7 +10,7 @@ var globals = {
         SPOTIFY_AUTH_ENDPOINT: 'https://accounts.spotify.com/authorize',
         SPOTIFY_API_ENDPOINT: 'https://api.spotify.com/v1/me',
         SPOTIFY_CLIENT_ID: '8cb840d28d554d4cb83e82213d283945',
-        REDIRECT_URI: 'http://www.sortzzi.io'
+        REDIRECT_URI: 'http://www.sortzzi.xyz'
     }
 };
 
