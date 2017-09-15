182b34063642edaf34d50f163ae2a976657ca41e
Eamonn Bell <pseudoboarder@gmail.com>
2017-09-15 15:11:09 +0100

    Remove Vuex

--- src/main.js
+++ src/main.js
@@ -1,17 +1,9 @@
 import localforage from 'localforage';
 import Vue from 'vue'
-import Vuex from 'vuex'
 import SpotifyWebApi from 'spotify-web-api-js'
 
 import App from './App.vue'
 
-Vue.use(Vuex);
-
-const store = new Vuex.Store({
-  state: {
-    loggedIn: false
-  }
-});
 
 var spotify = new SpotifyWebApi();
 Object.defineProperty(Vue.prototype, '$spotify', {value: spotify});
